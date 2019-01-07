8b328841370c3ef7716e77a36554639170bb69b2
think <microhuang@live.com>
2019-01-07 14:59:18 +0800

    Create weixin.py

---
+++ weixin.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#author: huang
+#date: 2018-04-20
+#comment: zabbix接入微信报警脚本
+#python huang 服务故障 无法连接服务器
+
+import requests
+import sys
+import os
+import json
+import logging
+
+import edata
+
+corpid=edata.corpid
+appsecret=edata.appsecret
+agentid=edata.agentid
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s, %(filename)s, %(levelname)s, %(message)s',
+                datefmt = '%a, %d %b %Y %H:%M:%S',
+                filename = os.path.join('/tmp/','weixin.log'),
+                filemode = 'a')
+
+#获取accesstoken
+token_url='https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpid + '&corpsecret=' + appsecret
+req=requests.get(token_url)
+accesstoken=req.json()['access_token']
+
+#发送消息
+msgsend_url='https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + accesstoken
+
+touser=sys.argv[1]
+subject=sys.argv[2]
+#toparty='3|4|5|6'
+message=sys.argv[3]
+
+params={
+        "touser": touser,
+#       "toparty": toparty,
+        "msgtype": "text",
+        "agentid": agentid,
+        "text": {
+                "content": message
+        },
+        "safe":0
+}
+
+req=requests.post(msgsend_url, data=json.dumps(params))
+
+logging.info('sendto:' + touser + ';;subject:' + subject + ';;message:' + message)
